50273e4f8562e1bb616cb3fb5c00281a196aab0f
fluttershy <fluttershydeveloper@gmail.com>
2017-05-23 21:01:48 +0200

    Add commands.lua
    Chat commands goes here until refactor

---
+++ commands.lua
@@ -0,0 +1,15 @@
+--# (project_name = 'funTasia BloX World')
+--# (project_alias = 'funtasia')
+--# (file_name = 'commands')
+--# (file_extension = 'lua')
+--# (file_format = 'lua script')
+--# (file_version = {major_version = 0, minor_version = 1, patch_version = 0, suffix_version = 'alpha'})
+--# [!] File version numbers have to match with that release version number where the file was last amended
+--# (author = {surname = 'Christian', lastname = 'Trant'})
+--# (date_of_creation = {day = 21, month = 05, year = 2017})
+--# (date_of_last_change = {day = 23, month = 05, year = 2017})
+--# (license = 'CC0')
+
+-- TODO: add chat command /funtasia version -> show version to player
+
+-- TODO: add chat command /funtasia OR /funtasia help -> show chat command help to player
